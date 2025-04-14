06d92583d0158802a62102f3d89775b581289075
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2025-04-14 17:10:54 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,18 @@ terraform {
       source = "huaweicloud/huaweicloud"
       version = ">= 1.73.0"
     }
+  },
+  backend "s3" {  
+    bucket = "w30037381-inner-tr-west-1"
+    key    = "w30037381-inner-tr-west-1/terraform.tfstate"  
+    region = "me-east-1"      
+    endpoint = "https://obs.me-east-1.myhuaweicloud.com"    
+    
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true    
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
